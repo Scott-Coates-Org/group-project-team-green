ad4f0d27a588e56445bb8d9df7076873dcd21c17
amarborz <amarborz@hotmail.com>
2022-06-25 22:07:16 +0300

    added side bar and navbar to admin ui

---
+++ src/components/admin/sideBar/SideBar.jsx
@@ -0,0 +1,38 @@
+import { NavItem, NavLink, Nav } from 'reactstrap'
+import { Link } from 'react-router-dom'
+
+import { sidebarItems } from './sideBarData'
+import SubMenu from './SubMenu'
+
+import styles from './sideBar.module.css'
+
+const SideBar = () => {
+	return (
+		<div className={styles.sideMenu}>
+			<div className={styles.sidebarItems}>
+				{sidebarItems.map((item) => {
+					const { name, icon, children } = item
+					return (
+						<div>
+							{children ? (
+								<SubMenu items={children} name={name} icon={icon} key={name} />
+							) : (
+								<div key={name} className={styles.sidebarItem}>
+									<Link
+										to={`/admin/${name.toLowerCase()}`}
+										className={styles.link}
+									>
+										<span className="mr-2">{icon}</span>
+										{name}
+									</Link>
+								</div>
+							)}
+						</div>
+					)
+				})}
+			</div>
+		</div>
+	)
+}
+
+export default SideBar
--- src/components/app.js
+++ src/components/app.js
@@ -1,146 +1,146 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "components/home";
-import { AuthProvider, useAuth } from "components/user/auth";
-import Login from "components/user/login";
-import Logout from "components/user/logout";
-import { firebase } from "firebase/client";
-import { createBrowserHistory } from "history";
-import { useEffect } from "react";
-import { Provider, useDispatch } from "react-redux";
-import { Route, Router, Switch } from "react-router-dom";
-import store from "redux/store";
-import { getData, getDataSuccess } from "redux/user";
-import ErrorBoundary from "components/error-boundary";
-import Admin from "./admin/dashboard";
-import Checkout from "./customer/checkout";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Home from 'components/home'
+import { AuthProvider, useAuth } from 'components/user/auth'
+import Login from 'components/user/login'
+import Logout from 'components/user/logout'
+import { firebase } from 'firebase/client'
+import { createBrowserHistory } from 'history'
+import { useEffect } from 'react'
+import { Provider, useDispatch } from 'react-redux'
+import { Route, Router, Switch } from 'react-router-dom'
+import store from 'redux/store'
+import { getData, getDataSuccess } from 'redux/user'
+import ErrorBoundary from 'components/error-boundary'
+import Checkout from './customer/checkout'
+import AdminPage from './admin/AdminPage'
 
 // DO NOT import BrowserRouter (as per tutorial). that caused router to not actually do anything.
 // see here: https://stackoverflow.com/questions/63554233/react-router-v5-history-push-changes-the-address-bar-but-does-not-change-the
 // https://github.com/ReactTraining/react-router/issues/4059#issuecomment-254437084
 // this is incredibly common but not our problem: https://stackoverflow.com/questions/62449663/react-router-with-custom-history-not-working
-export const history = createBrowserHistory();
+export const history = createBrowserHistory()
 
 function withReduxProvider(Component) {
-  return function withReduxProvider(props) {
-    return (
-      <Provider store={store}>
-        <Component {...props} />
-      </Provider>
-    );
-  };
+	return function withReduxProvider(props) {
+		return (
+			<Provider store={store}>
+				<Component {...props} />
+			</Provider>
+		)
+	}
 }
 
 function App() {
-  const props = {};
+	const props = {}
 
-  const dispatch = useDispatch();
+	const dispatch = useDispatch()
 
-  useEffect(() => {
-    dispatch(getData());
-  }, []);
+	useEffect(() => {
+		dispatch(getData())
+	}, [])
 
-  const storeUserData = (user) => {
-    const providerData = user.providerData[0];
-    user.admin && console.info("The user is admin!");
+	const storeUserData = (user) => {
+		const providerData = user.providerData[0]
+		user.admin && console.info('The user is admin!')
 
-    const userData = { ...providerData, uid: user.uid };
+		const userData = { ...providerData, uid: user.uid }
 
-    dispatch(getDataSuccess(userData));
-  };
+		dispatch(getDataSuccess(userData))
+	}
 
-  const appElement = (
-    <ErrorBoundary>
-      <AuthProvider onLogin={storeUserData}>
-        <Router history={history}>
-          <Switch>
-            <Route
-              path="/login"
-              render={(routeProps) => (
-                <Login {...routeProps} {...props} firebase={firebase} />
-              )}
-            />
-            <Route
-              path="/logout"
-              render={(routeProps) => (
-                <Logout {...routeProps} {...props} firebase={firebase} />
-              )}
-            />
+	const appElement = (
+		<ErrorBoundary>
+			<AuthProvider onLogin={storeUserData}>
+				<Router history={history}>
+					<Switch>
+						<Route
+							path="/login"
+							render={(routeProps) => (
+								<Login {...routeProps} {...props} firebase={firebase} />
+							)}
+						/>
+						<Route
+							path="/logout"
+							render={(routeProps) => (
+								<Logout {...routeProps} {...props} firebase={firebase} />
+							)}
+						/>
 
-            {/* this must be on the bottom */}
-            <ProtectedRoute
-              isAdminRoute
-              path="/admin"
-              component={Admin}
-              {...props}
-            />
-            <ProtectedRoute path="/checkout" component={Checkout} {...props} />
-            <ProtectedRoute path="/" component={Home} {...props} />
-          </Switch>
-        </Router>
-      </AuthProvider>
-    </ErrorBoundary>
-  );
+						{/* this must be on the bottom */}
+						<ProtectedRoute
+							isAdminRoute
+							path="/admin"
+							component={AdminPage}
+							{...props}
+						/>
+						<ProtectedRoute path="/checkout" component={Checkout} {...props} />
+						<ProtectedRoute path="/" component={Home} {...props} />
+					</Switch>
+				</Router>
+			</AuthProvider>
+		</ErrorBoundary>
+	)
 
-  return appElement;
+	return appElement
 }
 
-const AppWithRedux = withReduxProvider(App);
-export default AppWithRedux;
+const AppWithRedux = withReduxProvider(App)
+export default AppWithRedux
 
 // https://github.com/auth0/auth0-react/blob/master/EXAMPLES.md#1-protecting-a-route-in-a-react-router-dom-app
 const ProtectedRoute = ({ component, ...args }) => {
-  const WrappedComponent = withAuthenticationRequired(component, {
-    onRedirecting: () => "resuming session…",
-  });
+	const WrappedComponent = withAuthenticationRequired(component, {
+		onRedirecting: () => 'resuming session…',
+	})
 
-  const retVal = (
-    <Route
-      render={(routeProps) => <WrappedComponent {...routeProps} {...args} />}
-    />
-  );
+	const retVal = (
+		<Route
+			render={(routeProps) => <WrappedComponent {...routeProps} {...args} />}
+		/>
+	)
 
-  return retVal;
-};
+	return retVal
+}
 
 // much of this was copied from auth0 lib
 // node_modules/@auth0/auth0-react/src/with-authentication-required.tsx
 function withAuthenticationRequired(Component, options) {
-  return function WithAuthenticationRequired(props) {
-    const { isAdminRoute = false } = props;
-    const { isAuthenticated, isLoaded, user } = useAuth();
-    const {
-      returnTo = defaultReturnTo,
-      onRedirecting = defaultOnRedirecting,
-      loginOptions = {},
-    } = options;
+	return function WithAuthenticationRequired(props) {
+		const { isAdminRoute = false } = props
+		const { isAuthenticated, isLoaded, user } = useAuth()
+		const {
+			returnTo = defaultReturnTo,
+			onRedirecting = defaultOnRedirecting,
+			loginOptions = {},
+		} = options
 
-    useEffect(async () => {
-      let isAuthorized = false;
+		useEffect(async () => {
+			let isAuthorized = false
 
-      if (isLoaded) {
-        isAuthorized = isAdminRoute ? !!user?.admin : isAuthenticated;
+			if (isLoaded) {
+				isAuthorized = isAdminRoute ? !!user?.admin : isAuthenticated
 
-        const opts = {
-          ...loginOptions,
-          appState: {
-            ...loginOptions.appState,
-            returnTo: typeof returnTo === "function" ? returnTo() : returnTo,
-          },
-        };
+				const opts = {
+					...loginOptions,
+					appState: {
+						...loginOptions.appState,
+						returnTo: typeof returnTo === 'function' ? returnTo() : returnTo,
+					},
+				}
 
-        if (!isAuthenticated) history.push("/login", opts);
-        else if (!isAuthorized) history.push("/", opts);
-      }
-    }, [history, isAuthenticated, loginOptions, returnTo]);
+				if (!isAuthenticated) history.push('/login', opts)
+				else if (!isAuthorized) history.push('/', opts)
+			}
+		}, [history, isAuthenticated, loginOptions, returnTo])
 
-    // If authenticated,
-    // Is the route admin only?
-    // If route is admin only, is the user admin?
-    return isAuthenticated ? <Component {...props} /> : onRedirecting();
-  };
+		// If authenticated,
+		// Is the route admin only?
+		// If route is admin only, is the user admin?
+		return isAuthenticated ? <Component {...props} /> : onRedirecting()
+	}
 }
 
 const defaultReturnTo = () =>
-  `${window.location.pathname}${window.location.search}`;
+	`${window.location.pathname}${window.location.search}`
 
-const defaultOnRedirecting = () => <></>;
+const defaultOnRedirecting = () => <></>
