4d03239aea5b09733b31366b7f4491f4f825944f
Umar Kantaev <amarborz@hotmail.com>
2022-07-01 12:40:55 +0300

    resolved #36 (#41)
    * removed eslint from package.json
    
    * fixed admin and checkout route in the app.js to make the repo work
    
    * resolved #36
    
    Added routes for admin page and dummy components for each route. also removed eslint and fixed some bugs
    
    * fixed addons route
    
    Co-authored-by: Jennifer Huynh <jntlhuynh@gmail.com>

---
+++ src/components/admin/AllRoutes.jsx
@@ -0,0 +1,41 @@
+import { Route, Switch } from "react-router-dom"
+
+import {
+  AllProducts,
+  Apps,
+  Bookings,
+  Categories,
+  CreateAddOns,
+  CreateProducts,
+  Customers,
+  Dashboard,
+  DiscountCodes,
+  Inbox,
+  Reports,
+  Settings,
+  Stock,
+} from "components/admin/adminRoutes"
+
+const AllRoutes = () => {
+  return (
+    <>
+      <Switch>
+        <Route path="/admin/dashboard" component={Dashboard} />
+        <Route path="/admin/inbox" component={Inbox} />
+        <Route path="/admin/bookings" component={Bookings} />
+        <Route path="/admin/all products" component={AllProducts} />
+        <Route path="/admin/create products" component={CreateProducts} />
+        <Route path="/admin/addons" component={CreateAddOns} />
+        <Route path="/admin/stock" component={Stock} />
+        <Route path="/admin/discount codes" component={DiscountCodes} />
+        <Route path="/admin/categories" component={Categories} />
+        <Route path="/admin/customers" component={Customers} />
+        <Route path="/admin/reports" component={Reports} />
+        <Route path="/admin/apps" component={Apps} />
+        <Route path="/admin/settings" component={Settings} />
+      </Switch>
+    </>
+  )
+}
+
+export default AllRoutes
---
+++ src/components/admin/adminRoutes/DiscountCodes.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const DiscountCodes = () => {
+  return <div>DiscountCodes</div>
+}
+
+export default DiscountCodes
