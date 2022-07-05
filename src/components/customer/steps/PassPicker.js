2af8375b214ea7f43ce5cf0e8dd41a4035f572f5
Jennifer Huynh <jntlhuynh@gmail.com>
2022-07-05 01:58:27 -0700

    displayed products and quantity

--- src/components/customer/steps/PassPicker.js
+++ src/components/customer/steps/PassPicker.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {
     Accordion,
     AccordionItem,
@@ -6,95 +6,116 @@ import {
     AccordionItemButton,
     AccordionItemPanel,
 } from 'react-accessible-accordion';
+import { Button, ButtonGroup, ButtonToolbar } from 'reactstrap';
 
 // Demo styles, see 'Styles' section below for some notes on use.
 // import 'react-accessible-accordion/dist/fancy-example.css';
 import './passpicker.css';
 import myImage from '../../../assets/images/homepage-background.png';
 import firebaseClient from '../../../firebase/client'
+import IncDecCounter from './IncDecCounter';
 
+const timeslots = [
+    "10:00 am", "10:30 am", "11:00 am", "11:30 am", "12:00 pm",
+    "12:30 pm", "1:00 pm", "1:30 pm", "2:00 pm", "2:30 pm",
+    "3:00 pm", "3:30 pm", "4:00 pm", "4:30 pm", "5:00 pm",
+    "5:30 pm", "6:00 pm", "6:30 pm", "7:00 pm"
+]
 
 const PassPicker = ({ pickedDate }) => {
-    let productsRef = firebaseClient
-        .firestore()
-        .collection("products")
-        .get()
-        .then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                console.log(doc.id, " => ", doc.data());
+    const [products, setProducts] = useState([]);
+    let productsQuantity = [];
+
+    useEffect(() => {
+        const getProductsFromFirebase = [];
+        const productsRef = firebaseClient
+            .firestore()
+            .collection("products")
+            .onSnapshot((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    getProductsFromFirebase.push({
+                        ...doc.data(),
+                        key: doc.id,
+                    });
+                });
+                setProducts(getProductsFromFirebase);
             });
-        });
 
+        return () => productsRef();
+    }, []);
 
-    // console.log(productsRef);
-    // console.log(_fetchProducts);
+    const createSessionTimes = (name, number) => {
+        let sessionTimes = [];
+        for (let i = 0; i < number; i++) {
+            sessionTimes.push(<Button key={`${name}-btn-${i}`}>{timeslots[i]}</Button>)
+        }
+        return sessionTimes;
+    }
+
+    const setQuantity = (name, qty) => {
+        let prod = productsQuantity.find(prod => prod.name === name);
+
+        if (!prod) {
+            productsQuantity.push({ name, qty });
+        } else {
+            prod.qty += qty;
+        }
+    }
+
     return (
         <Accordion allowZeroExpanded allowMultipleExpanded>
-            <AccordionItem className='accordion__item'>
-                <AccordionItemHeading className=''>
+            {products.map((product) =>
+                <div key={product.key}>
+                    <AccordionItem className='accordion__item'>
+                        <AccordionItemHeading >
+                            <AccordionItemButton>
+                                <div className='accordion__heading'>
+                                    <img src={product.Photo} className='img' />
+                                    <div>
+                                        <h3>{product.Name}</h3>
+                                        {setQuantity(product.Name, 0)}
+                                        <p>{`${product.Duration} minutes!`}</p>
+                                    </div>
+                                </div>
 
-                    <AccordionItemButton>
-                        <div className='accordion__heading'>
-                            <img src={myImage} className='img' />
+                            </AccordionItemButton>
+                        </AccordionItemHeading>
+                        <AccordionItemPanel className='accordion__panel'>
                             <div>
-                                <h3>Unlimited Pass</h3>
-                                <p>All day pass!</p>
+                                {product.Name === 'Jr. Jump Chaperone' ? (
+                                    <div>
+                                        <p>Session Time</p>
+                                        <div className='session-time-btns'>
+                                            {createSessionTimes('jr-chaperone', 4)}
+                                        </div>
+                                        <div className='pass-quantity-selection'>
+                                            <p>Junior Jump Parent</p>
+                                            <p>2 hours</p>
+                                            <p>{`$${product.Price}.00`}</p>
+                                            <IncDecCounter />
+                                        </div>
+                                    </div>
+                                ) : null}
+                                {product.Name === 'Junior Jump Pass 120 Min' ? (
+                                    <div>
+                                        <p>Session Time</p>
+                                        <div className='session-time-btns'>
+                                            {createSessionTimes('jr-kids', 4)}
+                                        </div>
+                                        <div className='pass-quantity-selection'>
+                                            <p>Junior Jumper 6 or Under</p>
+                                            <p>2 hours</p>
+                                            <p>{`$${product.Price}.00`}</p>
+                                            <IncDecCounter />
+                                        </div>
+                                    </div>
+                                ) : null}
                             </div>
-                        </div>
+                        </AccordionItemPanel>
+                    </AccordionItem>
+                </div>
 
-                    </AccordionItemButton>
-                </AccordionItemHeading>
-                <AccordionItemPanel className='accordion__panel'>
-                    <p>
-                        Access to all Park attractions that are open!
-
-                        Altitude Safety Socks Required
-
-                    </p>
-                </AccordionItemPanel>
-            </AccordionItem>
-            <AccordionItem className='accordion__item'>
-                <AccordionItemHeading>
-                    <AccordionItemButton>
-                        <div className='accordion__heading'>
-                            <img src={myImage} className='img' />
-                            <div>
-                                <h3>Power Pass</h3>
-                                <p>120 Minutes Jump Pass</p>
-                            </div>
-                        </div>
-                    </AccordionItemButton>
-                </AccordionItemHeading>
-                <AccordionItemPanel className='accordion__panel'>
-                    <p>
-                        120 Minutes of Jump Time! Access to all Park Features
-                        that are open!
-
-                        Altitude Safety Socks Required!
-                    </p>
-                </AccordionItemPanel>
-            </AccordionItem>
-            <AccordionItem className='accordion__item'>
-                <AccordionItemHeading>
-                    <AccordionItemButton>
-                        <div className='accordion__heading'>
-                            <img src={myImage} className='img' />
-                            <div>
-                                <h3>Junior Jumpers Jam</h3>
-                                <p>Jump Pass for children 6 or Under</p>
-                            </div>
-                        </div>
-                    </AccordionItemButton>
-                </AccordionItemHeading>
-                <AccordionItemPanel className='accordion__panel'>
-                    <p>
-                        Jump area for children 6 or Under!
-
-                        Altitude Safety Socks Required
-
-                    </p>
-                </AccordionItemPanel>
-            </AccordionItem>
+            )}
         </Accordion>
     )
 }
