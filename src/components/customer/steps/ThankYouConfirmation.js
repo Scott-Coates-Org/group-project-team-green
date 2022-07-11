f92e95ff5ffaa29f2d66e849c2f20f60969c5c8c
Jennifer Huynh <jntlhuynh@gmail.com>
2022-07-10 23:08:01 -0700

    placholder confirmation page

--- src/components/customer/steps/ThankYouConfirmation.js
+++ src/components/customer/steps/ThankYouConfirmation.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useLocation } from 'react'
 
 const ThankYouConfirmation = () => {
   return (
-    <div>ThankYouConfirmation</div>
+    <div className='d-flex flex-column justify-content-center align-items-center'>
+      <h1>Thank you for your purchase!</h1>
+      <h3>Confirmation #111111</h3>
+      <div>
+        <p>Insert booking details here</p>
+        <p>Date of Transaction</p>
+      </div>
+    </div>
   )
 }
 
