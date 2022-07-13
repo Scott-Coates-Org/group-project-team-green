a4dc7cffb7eb9f7fe9094ab256a6fda036f096c0
E. Berke KARAGÖZ <34271483+BerkeKaragoz@users.noreply.github.com>
2022-07-14 02:28:47 +0300

    add modals

--- src/components/admin/adminRoutes/Reports.jsx
+++ src/components/admin/adminRoutes/Reports.jsx
@@ -1,7 +1,14 @@
 import React from "react"
 import { useTable, usePagination } from "react-table"
 import { format as formatDate } from "date-fns"
-import { Button, Table } from "reactstrap"
+import {
+  Button,
+  Table,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "reactstrap"
 import s from "./reports.module.css"
 //import { useDispatch } from "react-redux"
 
@@ -10,7 +17,7 @@ const date = new Date()
 const storeData = [...new Array(100)].map((e) => ({
   bookingDate: formatDate(date, "dd MMMM yyyy"),
   sessionTime: formatDate(date, "p"),
-  bookingId: <a href="#">{Math.round(Math.random() * 1000000)}</a>,
+  bookingId: Math.round(Math.random() * 1000000),
   headcount: Math.round(Math.random() * 15),
   amount: Math.round(Math.random() * 100),
   balance: Math.round(Math.random() * 1000),
@@ -52,6 +59,10 @@ const ReportsTable = ({
     usePagination
   )
 
+  const modalDataIndex = React.useRef(0)
+  const [modal, setModal] = React.useState(false)
+  const toggle = () => setModal(!modal)
+
   // Listen for changes in pagination and use the state to fetch our new data
   React.useEffect(() => {
     getData({ pageIndex, pageSize })
@@ -59,7 +70,7 @@ const ReportsTable = ({
 
   return (
     <div className="bg-white">
-      <Table size="sm" hover={true} {...getTableProps()}>
+      <Table size="sm" hover {...getTableProps()}>
         <thead>
           {headerGroups.map((headerGroup) => (
             <tr {...headerGroup.getHeaderGroupProps()}>
@@ -76,15 +87,19 @@ const ReportsTable = ({
         </thead>
         <tbody {...getTableBodyProps()}>
           {page.map((row, i) => {
+            const rowId = row.values.bookingId
             prepareRow(row)
             return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  return (
-                    <td {...cell.getCellProps()} style={{ cursor: "pointer" }}>
-                      {cell.render("Cell")}
-                    </td>
-                  )
+              <tr
+                {...row.getRowProps()}
+                style={{ cursor: "pointer" }}
+                onClick={() => {
+                  modalDataIndex.current = i
+                  toggle()
+                }}
+              >
+                {row.cells.map((cell, i) => {
+                  return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
                 })}
               </tr>
             )
@@ -95,8 +110,9 @@ const ReportsTable = ({
         <div>
           {[...new Array(pageCount)].map((_, i) => (
             <Button
+              key={"pagecount-" + i}
               onClick={() => gotoPage(i)}
-              color={pageIndex === i ? "primary" : "secondary"}
+              active={pageIndex === i}
             >
               {i + 1}
             </Button>
@@ -113,8 +129,8 @@ const ReportsTable = ({
           </span>
           {[20, 50, 100, 200].map((num) => (
             <Button
-              key={num}
-              color={pageSize === num ? "primary" : "secondary"}
+              key={"pagesize-" + num}
+              active={pageSize === num}
               onClick={() => {
                 setPageSize(num)
               }}
@@ -124,6 +140,25 @@ const ReportsTable = ({
           ))}
         </div>
       </div>
+      <Modal isOpen={modal} toggle={toggle}>
+        {data[modalDataIndex.current] && (
+          <>
+            <ModalHeader toggle={toggle}>
+              {data[modalDataIndex.current].bookingName} #
+              {data[modalDataIndex.current].bookingId}
+            </ModalHeader>
+            <ModalBody>{data[modalDataIndex.current].bookingId}</ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={toggle}>
+                Do Something
+              </Button>{" "}
+              <Button color="secondary" onClick={toggle}>
+                Cancel
+              </Button>
+            </ModalFooter>
+          </>
+        )}
+      </Modal>
     </div>
   )
 }
